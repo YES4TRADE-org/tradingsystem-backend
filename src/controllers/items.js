da4a3212a4f2e2c7a99d10ddb4142d3c1e05cf6a
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-23 00:48:40 +0800

    fix a getbooks route

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -5,7 +5,12 @@ export const router = express.Router();
 
 router.get('/yes4trade/getbooks', async (req, res) => {
     const data = await getBooks();
-    const books  = data.rows;
+    const books  = data.rows.map(book => {
+        return {
+            ...book,
+            image: book.image? book.image.toString('base64') : null
+        };
+    });
     console.log(books);
     return res.status(200).json(books);
 });
