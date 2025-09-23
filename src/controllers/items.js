7fe761a6e8e5d91dac0e44e6d2b4baff9014ce63
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-23 23:02:23 +0800

    seperate the file of upload and items

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -13,20 +13,4 @@ router.get('/yes4trade/getbooks', async (req, res) => {
     return res.status(200).json(books);
 });
 
-router.post('/yes4trade/upload', upload.single('image'), async (req, res) => {
-    const { title, requirements, methods } = req.body;
-    try {
-       const file = await cloudinary.cloudinary.uploader.upload_stream({ folder: 'yes4trade' }, 
-       (error, result) => {
-           if (error) {
-               console.error('Error uploading to Cloudinary:', error);
-               return res.status(500).json({ error: 'Failed to upload image' });
-           }
-           uploadImage(title, requirements, methods, result.secure_url);
-            return res.status(200).json({ message: 'Image uploaded successfully', url: result.secure_url });
-       });
-    } catch (error) {
-        console.error('Error cannot upload file', error);
-        return res.status(500).json({ error: 'Failed to upload image' });
-    }   
-})
+
---
+++ src/controllers/upload.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import multer from 'multer';
+import { uploadImage } from '../services/upload-image.js';
+
+export const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post('/yes4trade/upload', upload.single('image'), async (req, res) => {
+    const { title, requirements, methods } = req.body;
+    try {
+       const file = await cloudinary.cloudinary.uploader.upload_stream({ folder: 'yes4trade' }, 
+       (error, result) => {
+           if (error) {
+               console.error('Error uploading to Cloudinary:', error);
+               return res.status(500).json({ error: 'Failed to upload image' });
+           }
+           uploadImage(title, requirements, methods, result.secure_url);
+            return res.status(200).json({ message: 'Image uploaded successfully', url: result.secure_url });
+       });
+    } catch (error) {
+        console.error('Error cannot upload file', error);
+        return res.status(500).json({ error: 'Failed to upload image' });
+    }   
+});
