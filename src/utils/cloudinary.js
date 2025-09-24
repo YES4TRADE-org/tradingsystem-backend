eb74de41bd53152864812fa08261875ba9ec9f93
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-25 02:29:11 +0800

    Remove node_modules and cloudinary.js from tracking

--- src/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({ 
-    cloud_name: 'dkacxbbwh', 
-    api_key: '182463543584545', 
-    api_secret: 'yRU21weJxQ4I_9EiydrY5V9qxMY' 
-});
-
-export default cloudinary;
