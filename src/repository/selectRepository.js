feb241f17e8f0154120af3a006e45003a6889159
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-25 00:13:21 +0800

    created a create post products logics in service

--- src/repository/selectRepository.js
+++ src/repository/selectRepository.js
@@ -1,13 +1,13 @@
 import { pool } from '../database/db.js';
 
-export default async function getProgramId(program) {
+export async function getProgramId(program) {
     return await pool.query('SELECT program_id FROM programs WHERE programs = $1', [program]);
 }
 
-export default async function getTypeId(type) {
+export async function getTypeId(type) {
     return await pool.query('SELECT type_id FROM types WHERE types = $1', [type]);
 }
 
-export default async function getMethodId(method) {
+export async function getMethodId(method) {
     return await pool.query('SELECT method_id FROM methods WHERE methods = $1', [method]);
 }
--- src/services/logics.js
+++ src/services/logics.js
@@ -1,2 +1,9 @@
-import selectRepository from '../repository/selectRepository.js';
+import {getMethodId, getProgramId, getTypeId} from '../repository/selectRepository.js';
 
+export async function createPostProduct(methods, program, type){
+    const method_id = await getMethodId(methods);
+    const program_id = await getProgramId(program);
+    const type_id = await getTypeId(type);
+
+    return {method_id, program_id, type_id};
+}
