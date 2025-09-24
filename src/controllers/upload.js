0b5f205340819ad0b434118987083bb0bb7953b5
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-25 00:21:27 +0800

    fix the post for upload-trade and able to send it to database

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import multer from 'multer';
-import { uploadImage } from '../repository/uploadRepository.js';
+import { postProduct } from '../repository/uploadRepository.js';
+import { createPostProduct } from '../services/logics.js';
 
 export const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/yes4trade/upload', upload.single('image'), async (req, res) => {
-    const { title, requirements, methods, email, studentId, program, type, price } = req.body;
+router.post('/yes4trade/upload-trade', upload.single('image'), async (req, res) => {
+    const { title, requirements, methods, email, studentId, program, type } = req.body;
+
+    const {method_id, program_id, type_id} = createPostProduct(methods, program, type);
 
     try {
        const file = await cloudinary.cloudinary.uploader.upload_stream({ folder: 'yes4trade' }, 
@@ -15,8 +18,8 @@ router.post('/yes4trade/upload', upload.single('image'), async (req, res) => {
                console.error('Error uploading to Cloudinary:', error);
                return res.status(500).json({ error: 'Failed to upload image' });
            }
-           uploadImage(title, requirements, methods, result.secure_url);
-            return res.status(200).json({ message: 'Image uploaded successfully', url: result.secure_url });
+           postProduct(title, requirements, result.secure_url, method_id, studentId, program_id, type_id);
+           return res.status(200).json({ message: 'Image uploaded successfully', url: result.secure_url });
        });
     } catch (error) {
         console.error('Error cannot upload file', error);
--- src/repository/uploadRepository.js
+++ src/repository/uploadRepository.js
@@ -1,8 +1,9 @@
 import { pool } from '../database/database.js';
 
-export async function uploadImage(title, requirements, methods , url) {
+export async function postProduct(title, requirements, url, price, methods, student_id, program, type) {
     return await pool.query(
-       'INSERT INTO trading_books (title, requirements, methods, url) VALUES ($1, $2, $3, $4)',
-        [title, requirements, methods, url] 
+       `INSERT INTO trading_products (title, requirements, url, price, methods, student_id, program, type) 
+        VALUES ($1, $2, $3, $4)`,
+        [title, requirements, url, price, methods, student_id, program, type] 
     );
 }
