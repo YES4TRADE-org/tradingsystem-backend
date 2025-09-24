0b55b3e53ceafb321f5d828cd396db5b2d5e637e
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-25 00:27:36 +0800

    created to routes for trading and selling

---
+++ src/controllers/upload-sale.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import multer from 'multer';
+import { postProduct } from '../repository/uploadRepository.js';
+import { createPostProduct } from '../services/logics.js';
+
+export const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post('/yes4trade/upload-sell', upload.single('image'), async (req, res) => {
+    const { title, methods, email, studentId, program, type, price} = req.body;
+
+    const {method_id, program_id, type_id} = createPostProduct(methods, program, type);
+
+    try {
+       const file = await cloudinary.cloudinary.uploader.upload_stream({ folder: 'yes4trade' }, 
+       (error, result) => {
+           if (error) {
+               console.error('Error uploading to Cloudinary:', error);
+               return res.status(500).json({ error: 'Failed to upload image' });
+           }
+           postProductTrade(title, result.secure_url, price, method_id, studentId, program_id, type_id);
+           return res.status(200).json({ message: 'Image uploaded successfully', url: result.secure_url });
+       });
+    } catch (error) {
+        console.error('Error cannot upload file', error);
+        return res.status(500).json({ error: 'Failed to upload image' });
+    }   
+});
--- src/repository/uploadRepository.js
+++ src/repository/uploadRepository.js
@@ -1,9 +1,17 @@
 import { pool } from '../database/database.js';
 
-export async function postProduct(title, requirements, url, price, methods, student_id, program, type) {
+export async function postProductTrade(title, requirements, url, methods, student_id, program, type) {
     return await pool.query(
        `INSERT INTO trading_products (title, requirements, url, price, methods, student_id, program, type) 
         VALUES ($1, $2, $3, $4)`,
-        [title, requirements, url, price, methods, student_id, program, type] 
+        [title, requirements, url, methods, student_id, program, type] 
+    );
+}
+
+export async function postProductSell(title, url, price, methods, student_id, program, type) {
+    return await pool.query(
+       `INSERT INTO trading_products (title, requirements, url, price, methods, student_id, program, type) 
+        VALUES ($1, $2, $3, $4)`,
+        [title, url, price, methods, student_id, program, type] 
     );
 }
