383e58c7fe617c39dcb8560ac7dd33c48f338b5b
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-28 16:41:17 +0800

    remove multer import since not being used

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { getAllPost } from '/tradingsystem-backend/src/repository/itemsRepository.js';
-import multer from 'multer';
 import { authenticated } from '../middlewares/authentication.js';
 
 export const itemsRouter = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
 
 itemsRouter .get('/yes4trade/getbooks', authenticated, async (req, res) => {
     const data = await getAllPost();
