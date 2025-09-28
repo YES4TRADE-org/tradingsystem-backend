98c8f6c05e47c669fa82eb38393ff2db610e0212
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-28 17:10:40 +0800

    create a syntax for checking database if the email is already used

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import { checkIfSlsu } from '../utils/help';
+import { checkEmail } from '../repository/userRepository';
 
 const signupRouter = express.Router();
 
 signupRouter.post('/yes4trade/auth/signup', (req, res) => {
     const { email, studentId, firstname, lastname, password, program} = req.body;
-    
+
+    if(checkIfSlsu(email)){
+        if(checkEmail(email) !== null){
+            return res.status(409).json({ message: 'You already have an account!'})
+        }
+         
+    } 
+
 });
 
 export default signupRouter;
--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,5 +1,7 @@
 import { pool } from '/tradingsystem-backend/src/database/database.js';
 
-export const checkEmail = () => {
-    
+export const checkEmail = (email) => {
+    return pool.query(
+        'SELECT * FROM users WHERE users.email = $1', [ email ]
+    );
 }
